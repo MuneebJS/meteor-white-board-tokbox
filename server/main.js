c1afe6b8d1b007b28c404e14f6df8c7b7b9e5985
M Muneeb <m.muneeb@nexgeni.net>
2017-10-10 15:33:27 +0500

    initial commit

---
+++ server/main.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/prefer-default-export */
+
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+/* export */ const FabricObjects = new Mongo.Collection('fabricObjects');
+
+Meteor.publish('fabricObjects', function fabricObjects() {
+  // if (!this.userId()) {
+    // this.ready();
+    // return [];
+  // }
+
+  return FabricObjects.find();
+});
+
+Meteor.methods({
+  clearCanvas() {
+    // if (! Meteor.userId()) {
+    //   throw new Meteor.Error("not-authorized");
+    // }
+
+    FabricObjects.remove({});
+  },
+});
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
